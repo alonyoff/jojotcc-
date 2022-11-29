54307e64efe55dbf22c897704e15f04941f65555
alonyoff <117363959+alonyoff@users.noreply.github.com>
2022-11-29 08:46:17 -0300

    Add files via upload

--- jojo.js
+++ jojo.js
@@ -203,7 +203,7 @@ function jojofav(){
         else if (nome.toLowerCase() == "gyro"){
             document.getElementById("h1").innerHTML = ("Gyro Zeppeli")
             document.getElementById("td").innerHTML = ("No começo atua como o protagonista, tendo mais destaque do que o proprio jojo. Entrou na corrida para salvar uma criança da execução. Até certa parte do manga, não possui stand mas isso não o impede de ser um exelente lutador com suas 'stell balls'. ")
-            document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIjnjeNhEX_LVFWQwC_dFFOMnms_lJ-Ytq-A&usqp=CAU'>";
+            document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://i.pinimg.com/736x/21/c2/a1/21c2a18dfade216e57f0163a737da609.jpg'>";
 
         }
 
@@ -218,7 +218,7 @@ function jojofav(){
         else if (nome.toLowerCase() == "yasuho"){
             document.getElementById("h1").innerHTML = ("Yasuho Hirose")
             document.getElementById("td").innerHTML = ("A jobro e interesse amoroso do josuke da parte 8. A jornada dos dois juntos começa quando ela o acha soterrado nas paredes oculares após o terremoto")
-            document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://i1.sndcdn.com/avatars-leV1nrvcOcV7JK8S-1tBxyQ-t500x500.jpg'>";
+            document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFJV5LPhYtUrkJtFsTro_KlAfc910yR-ASJQ&usqp=CAU'>";
 
         }
 
@@ -244,6 +244,12 @@ function jojofav(){
             document.getElementById("td").innerHTML = ("O avaliador de plantas pessoal da familia higashikata, vive em um teleferico nas montanhas. É um personagem fundamental para o desenrolar da obra, ele é o responsavel por encontrar o ramo da rokakaka no jardim higashikata")
             document.getElementById("img").innerHTML = "<img class='imgdp' src='https://static.wikia.nocookie.net/jojo/images/3/31/Rai_infobox.jpg/revision/latest?cb=20210122172346&path-prefix=es'>";
 
+        }
+
+
+        else if(nome.toLowerCase() == "amongus"){
+            document.getElementById("img").innerHTML = "<img class='imgdp' src ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaY2C95TqmqnYeqFCyWgX4Wb6DM_HeJDRsc0tW884VYQ&s'>";
+            document.getElementById("h1").innerHTML = ("DRIP AMONGUS")
         }
 
 
@@ -378,7 +384,7 @@ function jojofavs(){
             document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiQ7U3JvSLrNNc90sWwhCi3Mhv-reyzqwWU9DT4sU2OwEtdQOj1dQMRfbRjv5eFzoB0kQ&usqp=CAU'>";
         }
 
-        else if (nome.toLowerCase() == "polnareff" || nome.toLowerCase() == "p"){
+        else if (nome.toLowerCase() == "polnareff" || nome.toLowerCase() == "jean pierre polnareff"){
             document.getElementById("h1").innerHTML = ("Jean Pierre Polnareff") 
             document.getElementById("td").innerHTML = ("Um frances que se aliou ao Dio no começo para se vingar de um um homem que matou sua irmã, após ter a semente de carne retirada de sua testa, se juntou aos crusaders ainda buscando vingança")
             document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://i.pinimg.com/736x/93/d2/b9/93d2b98345cb92667c846493a368bfd0.jpg'>";
@@ -390,31 +396,31 @@ function jojofavs(){
             document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://i.pinimg.com/564x/9c/23/fb/9c23fb3fea95ce560ed99eb44ac99041.jpg'>";
         }
 
-        else if (nome.toLowerCase() == "okuyasu"){
+        else if (nome.toLowerCase() == "okuyasu nijimura"){
             document.getElementById("h1").innerHTML = ("Okuyasu Nijimura") 
             document.getElementById("td").innerHTML = ("Após tentar matar Josuke, o ajuda e então no dia seguinte aparece na casa do jojo como se nada tivesse acontecido.Tem um stand muito forte mas não tem a inteligencia necessaria para utilizar bem")
             document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://i1.sndcdn.com/avatars-c8jMegiCMzkv4PiP-aqgy5w-t500x500.jpg'>";
         }
 
-        else if (nome.toLowerCase() == "koichi"){
+        else if (nome.toLowerCase() == "koichi hirose"){
             document.getElementById("h1").innerHTML = ("Koichi Hirose") 
             document.getElementById("td").innerHTML = ("Um personagem que se desenvolve muito durante a obra, isso é refletido no seu stand que evolui.Um personagem que é muito bondoso e inocente.Conquistou o respeito de Jotaro ao revelar a identidade de Kira")
             document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC-0-kRuSQ1C8mMng1O4bdLeJqTtJ0p9mWX8WyxhGJ1mLF3gkZRN4dJejh1YMnl2x8WPI&usqp=CAU'>";
         }
 
-        else if (nome.toLowerCase() == "bucciarati"){
+        else if (nome.toLowerCase() == "bruno bucciarati"){
             document.getElementById("h1").innerHTML = ("Bruno Bucciarati ") 
             document.getElementById("td").innerHTML = ("Um dos jobros do Giorno, em sua primeira aparição tentou matar o protagonista, mas após ver que Giorno tem um coração puro, decide aceita-lo em sua gangue")
             document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxDy_KN58W3cWCwZ7xxLG0NaGCuH808bSty2PZ7_kWzq38N9Azsj6-q8hiMmp469gwbhA&usqp=CAU'>";
         }
 
-        else if(nome.toLowerCase() == "fugo"){
+        else if(nome.toLowerCase() == "pannacotta fugo"){
             document.getElementById("h1").innerHTML = ("Pannacotta Fugo") 
             document.getElementById("td").innerHTML = ("Um personagem com muito potencial, com um stand muito forte mas que não teve muito aproveitamento")
             document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://static.wikia.nocookie.net/jjba/images/4/4b/Pannacotta_Fugo_Anime.png/revision/latest?cb=20181213225942&path-prefix=pt-br'>";
         }
 
-        else if (nome.toLowerCase() == "abbacchio"){
+        else if (nome.toLowerCase() == "leone abbacchio"){
             document.getElementById("h1").innerHTML = ("Leone Abbacchio")
             document.getElementById("td").innerHTML = ("Um dos melhores personagens da parte 5. De primeira vista pode parecer apenas um emo revoltado, mas conforme a historia passa, percebemos o motivo dele ser assim. Ele não gosta de Giorno pois o protagonista o lembra de si quando era jovem e tinha um enorme senso de justiça")
             document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://s11.favim.com/orig/7/773/7737/77378/jojos-bizarre-adventure-leone-abbacchio-anime-icons-Favim.com-7737862.jpg'>";
@@ -428,21 +434,21 @@ function jojofavs(){
 
         }
 
-        else if (nome.toLowerCase() == "mista"){
+        else if (nome.toLowerCase() == "guido mista"){
             document.getElementById("h1").innerHTML = ("Guido Mista")
             document.getElementById("td").innerHTML = ("Um jovem que odeia o numero 4. Seu stand é numerado do 1 ao 7 mas pula do 3 para o 5. Foi um dos poucos sobreviventes da parte 5")
-            document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://static.wikia.nocookie.net/jjba/images/5/59/Shigechi_diu_color_v8_042.png/revision/latest?cb=20150215153249&path-prefix=pt-br'>";
+            document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://pbs.twimg.com/media/ELy925_XUAA6yAz.jpg'>";
 
         }
 
-        else if (nome.toLowerCase() == "narancia"){
+        else if (nome == "Narancia Ghirga"){
             document.getElementById("h1").innerHTML = ("Narancia Ghirga")
             document.getElementById("td").innerHTML = ("Narancia é um adolescente de média estatura e figura desajeitada.Na infancia foi salvo por Bucciarati e anos mais tarde entrou para sua guangue")
             document.getElementById("img").innerHTML = "<img class='imgdp' src = 'http://pm1.narvii.com/7435/18d31ae3a1017dcfd3095e00232b36095d571c7ar1-640-624v2_uhq.jpg'>";
 
         }
 
-        else if (nome.toLowerCase() == "hermes"){
+        else if (nome.toLowerCase() == "hermes costello"){
             document.getElementById("h1").innerHTML = ("Hermes Costello")
             document.getElementById("td").innerHTML = ("Conheceu e ajudou Jolyne na prisão. Apesar de estar disposta a ajudar jolyne, tem seus proprios objetivos. Infelizmente passou grande parte de Stone ocean na enfermaria ")
             document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://i.pinimg.com/474x/fc/56/83/fc5683cade5fb5158770aed7e2e32f61.jpg'>";
@@ -463,14 +469,14 @@ function jojofavs(){
 
         }
 
-        else if (nome.toLowerCase() == "anasui"){
+        else if (nome.toLowerCase() == "narciso anasui"){
             document.getElementById("h1").innerHTML = ("Narciso Anasui ")
             document.getElementById("td").innerHTML = ("Foi preso por 'desmontar' sua namorada. Se apaixonou por Jolyne no instante em que a viu e esta disposto a fazer qualquer coisa por ela. Em sua primeira aparição no manga era uma mulher, mas mudou nos capitulos seguintes")
             document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://images.8tracks.com/cover/i/012/408/854/tumblr_olh8caZ7ZL1rkd3u1o1_540-8215.png?rect=0,197,415,415&q=98&fm=jpg&fit=max&w=960&h=960'>";
 
         }
 
-        else if (nome.toLowerCase() == "emporio"){
+        else if (nome.toLowerCase() == "emporio alnino"){
             document.getElementById("h1").innerHTML = ("Emporio Alnino ")
             document.getElementById("td").innerHTML = ("Responsavel por alertar Jolyne dos problemas que estão por vir. Nasceu e cresceu na prisão, se escondendo em seu stand 'Burning Down the House' que é um quarto fantasma. Foi o responsavel pela vitoria dos herois na parte 6")
             document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://i.pinimg.com/originals/8b/ea/cd/8beacd080893981f266a7802ceee5401.jpg'>";
@@ -478,14 +484,14 @@ function jojofavs(){
         }
 
 
-        else if (nome.toLowerCase() == "gyro"){
+        else if (nome.toLowerCase() == "gyro zeppeli"){
             document.getElementById("h1").innerHTML = ("Gyro Zeppeli")
             document.getElementById("td").innerHTML = ("No começo atua como o protagonista, tendo mais destaque do que o proprio jojo. Entrou na corrida para salvar uma criança da execução. Até certa parte do manga, não possui stand mas isso não o impede de ser um exelente lutador com suas 'stell balls'. ")
-            document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIjnjeNhEX_LVFWQwC_dFFOMnms_lJ-Ytq-A&usqp=CAU'>";
+            document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://i.pinimg.com/736x/21/c2/a1/21c2a18dfade216e57f0163a737da609.jpg'>";
 
         }
 
-        else if (nome.toLowerCase() == "lucy"){
+        else if (nome.toLowerCase() == "lucy steel"){
             document.getElementById("h1").innerHTML = ("Lucy Steel")
             document.getElementById("td").innerHTML = ("A versão alternativa de Erina Pendleton. Casada com Stephen Steel, se juntou aos protagonistas para derrotar o presidente e salvar seu marido das constantes ameaças. Retorna na parte 8 como uma idosa")
             document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://64.media.tumblr.com/689fa653ae58d93ab620d2e2407a35a5/c19a7d8fece565fb-93/s1280x1920/84a14bdd08b76f7ae21d256ae79655356eefaa63.jpg'>";
@@ -493,22 +499,22 @@ function jojofavs(){
         }
 
 
-        else if (nome.toLowerCase() == "yasuho"){
+        else if (nome.toLowerCase() == "yasuho hirose"){
             document.getElementById("h1").innerHTML = ("Yasuho Hirose")
             document.getElementById("td").innerHTML = ("A jobro e interesse amoroso do josuke da parte 8. A jornada dos dois juntos começa quando ela o acha soterrado nas paredes oculares após o terremoto")
-            document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://i1.sndcdn.com/avatars-leV1nrvcOcV7JK8S-1tBxyQ-t500x500.jpg'>";
+            document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFJV5LPhYtUrkJtFsTro_KlAfc910yR-ASJQ&usqp=CAU'>";
 
         }
 
 
-        else if (nome.toLowerCase() == "joshu"){
+        else if (nome.toLowerCase() == "joshu higashikata"){
             document.getElementById("h1").innerHTML = ("Joshu Higashikata")
             document.getElementById("td").innerHTML = ("Não importa o quanto tente, ele nunca vai ser um jobro")
             document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://pm1.narvii.com/6958/403284957df784c0b3f254d18cce19354b810b74r1-320-320v2_hq.jpg'>";
 
         }
 
-        else if (nome.toLowerCase() == "tsurugi"){
+        else if (nome.toLowerCase() == "tsurugi higashikata"){
             document.getElementById("h1").innerHTML = ("Tsurugi Higashikata")
             document.getElementById("td").innerHTML = ("Filho de Jobin, esta em busca de uma cura para sua doença.Apesar de ser um menino, se veste como mulher para evitar uma maldição ")
             document.getElementById("img").innerHTML = "<img class='imgdp' src = 'https://i.pinimg.com/564x/04/a7/dd/04a7dd57598fbe2120f9dfa87defa27e.jpg'>";
@@ -516,7 +522,7 @@ function jojofavs(){
         }
 
 
-        else if (nome.toLowerCase() == "mamezuku rai" || nome.toLowerCase() =="mamezuku" || nome.toLowerCase() =="rai" || nome.toLowerCase == "rai mamezuku"){
+        else if (nome.toLowerCase() == "rai mamezuku" || nome.toLowerCase() =="mamezuku" || nome.toLowerCase() =="rai" || nome.toLowerCase == "rai mamezuku"){
 
             document.getElementById("h1").innerHTML = ("Rai Mamezuku")
             document.getElementById("td").innerHTML = ("O avaliador de plantas pessoal da familia higashikata, vive em um teleferico nas montanhas. É um personagem fundamental para o desenrolar da obra, ele é o responsavel por encontrar o ramo da rokakaka no jardim higashikata")
